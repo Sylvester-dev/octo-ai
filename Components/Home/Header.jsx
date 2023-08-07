5f8d5fb60e9fad9275204244e84b911c6e1ac888
Vineet Kumar <vineetkumar@Vineets-MacBook-Air.local>
2023-08-07 16:33:51 +0530

    navbar made

--- Components/Home/Header.jsx
+++ Components/Home/Header.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
+  const menuList = [
+    {
+      name: "Home",
+      link: "/",
+      style: "active",
+    },
+    {
+      name: "Chat",
+      link: "/chat",
+      style: "",
+    },
+  ];
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <button
+        className="navbar-toggler d-xl-none d-inline navbar-menu-button"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#primaryMenu"
+      >
+        <span className="navbar-toggler-icon">
+          <HiMenuAlt3 />
+        </span>
+      </button>
+      <a href="index.html">
+        <img src="assets/images/logo.svg" className="img-fluid" alt="logo" />
+      </a>
+      <nav className="header-nav-middle">
+        <div className="main-nav navbar navbar-expand-xl navbar-light navbar-sticky">
+          <div className="offcanvas offcanvas-collapse order-xl-2" id="primaryMenu">
+            <div className="offcanvas-header navbar-shadow">
+              <h5 className="mb-0">Back</h5>
+              <button
+                className="btn-close lead"
+                type="button"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav">
+                {menuList.map((menu, i) => (
+                  <li className={`nav-item ${menu.style}`}>
+                    <a className="nav-link" href={`${menu.link}`}>
+                      {menu.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <a
+        data-cursor="pointer"
+        href="/login"
+        className="btn btn-theme d-sm-inline-block d-none"
+      >
+        <span>Login Now</span>
+      </a>
+      <span className="new_space"></span>
+     
+       
+        <a
+          data-cursor="pointer"
+          className="btn btn-theme d-sm-inline-block d-none"
+        >
+          <span>Connect</span>
+        </a>
+      )
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Header } from '../Components/Home'
 
 const index = () => {
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default index
