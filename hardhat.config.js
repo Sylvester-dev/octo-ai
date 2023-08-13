8200c7295b7b2997952895fb2011056b38ef1e75
Vineet Kumar <vineetkumar@Vineets-MacBook-Air.local>
2023-08-13 18:33:32 +0530

    contract deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "polygon_mumbai",
   solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 31337,
     },
-    polygon_mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  },
-  etherscan: {
-    apiKey: process.env.POLYGONSCAN_API_KEY
   },
 };
-
-
-// GPTMembership: 0x5FbDB2315678afecb367f032d93F642f64180aa3
-//GPTMembership polygon: 0x373917004d664117D6e04b27f2EE7392D1e65117
